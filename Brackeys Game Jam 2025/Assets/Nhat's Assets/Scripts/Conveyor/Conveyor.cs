ca6651158a8de3fe601c1904bf4fd9bdd5251f1b
Pacnatz <nhatchu04@yahoo.com>
2025-02-17 11:25:58 -0600

    Simple Player Move & Converyor

---
+++ Brackeys Game Jam 2025/Assets/Nhat's Assets/Scripts/Conveyor/Conveyor.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Conveyor : MonoBehaviour {
+
+    public float conveyorSpeed = 2f;
+
+    private List<BaseItem> itemsOnConveyor = new List<BaseItem>();
+
+
+    private void Update() {
+        
+        foreach (BaseItem item in itemsOnConveyor) {
+            // If item is on conveyor
+            if (item.getOnConveyor()) {
+                item.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(conveyorSpeed, 0);
+            }
+        }
+
+    }
+
+
+    public void OnTriggerStay2D(Collider2D collision) {
+
+        if (collision.gameObject.TryGetComponent<BaseItem>(out var item)) {
+
+            // If item is not on conveyor
+            if (!item.getOnConveyor()) {
+                item.setOnConveyor(true);
+                collision.transform.position = new Vector3(collision.transform.position.x, transform.position.y, collision.transform.position.z);
+                itemsOnConveyor.Add(item);
+            }
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        
+        if (collision.gameObject.TryGetComponent<BaseItem>(out var item)) {
+            item.setOnConveyor(false);
+            item.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            itemsOnConveyor.Remove(item);
+        }
+    }
+}
--- Brackeys Game Jam 2025/Assets/Nhat's Assets/Scripts/DragScript.cs
+++ Brackeys Game Jam 2025/Assets/Nhat's Assets/Scripts/DragScript.cs
@@ -3,20 +3,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class DragScript : MonoBehaviour {
 
+    [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private LayerMask itemsLayer;
     [SerializeField] private LayerMask selectedItemLayer;
 
+    private PlayerInput playerInput;
+    private Mouse mouse;
+
+    private float playerSpeed = 5f;
+    private float playerAcceleration = 10f;
+
     private GameObject selectedItem;
     private Vector3 offset;
-    private Mouse mouse;
     private Vector2 worldPos;
 
     private void Start() {
-        //itemsLayer = LayerMask.NameToLayer("Items");
-        //selectedItemLayer = LayerMask.NameToLayer("SelectedItem");
+        playerInput = new PlayerInput();
+        playerInput.Player.Enable();
     }
 
     private void Update() {
+
+        // Mouse logic
         mouse = Mouse.current;
         if (mouse != null) {
             worldPos = Camera.main.ScreenToWorldPoint(mouse.position.ReadValue());
@@ -32,6 +40,27 @@ public class DragScript : MonoBehaviour {
                 selectedItem.transform.position = Camera.main.ScreenToWorldPoint(mouse.position.ReadValue()) + offset;
             }
         }
+
+        // 1D Keyboard logic
+        float moveDirX = playerInput.Player.PlayerMove.ReadValue<float>();
+        if (moveDirX != 0) {
+
+            // Decelerate twice as fast if moving in opposite directions
+            if (playerRb.linearVelocityX > moveDirX) {
+                playerRb.linearVelocityX = Mathf.MoveTowards(playerRb.linearVelocityX, moveDirX * playerSpeed, playerAcceleration * 2 * Time.deltaTime);
+            }
+            else if (playerRb.linearVelocityX < moveDirX) {
+                playerRb.linearVelocityX = Mathf.MoveTowards(playerRb.linearVelocityX, moveDirX * playerSpeed, playerAcceleration * 2 * Time.deltaTime);
+            }
+            else {
+                playerRb.linearVelocityX = Mathf.MoveTowards(playerRb.linearVelocityX, moveDirX * playerSpeed, playerAcceleration * Time.deltaTime);
+            }
+        }
+        else {
+            playerRb.linearVelocityX = Mathf.MoveTowards(playerRb.linearVelocityX, 0, playerAcceleration * 2 * Time.deltaTime);
+        }
+
+
     }
 
 
@@ -48,6 +77,7 @@ public class DragScript : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero, 100, itemsLayer);
         if (hit) {
             // Call base item script's merge function
+
             Debug.Log(hit.collider.gameObject.name);
         }
 
