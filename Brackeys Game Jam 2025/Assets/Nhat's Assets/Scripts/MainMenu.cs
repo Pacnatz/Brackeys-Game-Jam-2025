01a96edfca2ff74dde6b74defa4ac4ef6fdd3fdd
Chans-pat <162397190+Chans-pat@users.noreply.github.com>
2025-02-21 17:56:17 -0600

    MainMenu
    MainMenu

--- Brackeys Game Jam 2025/Assets/Nhat's Assets/Scripts/MainMenu.cs
+++ Brackeys Game Jam 2025/Assets/Nhat's Assets/Scripts/MainMenu.cs
@@ -1,16 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    string MainMenu_Scene = "MainMenu";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  public  void Mainmenu()
     {
-        
+        Debug.Log("Load MainMenu");
+        SceneManager.LoadScene(MainMenu_Scene);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
+
+    
 }
